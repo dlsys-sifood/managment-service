88c2067c93916e5783a9d00a6a12280634df019b
salvador5120 <salvador.delaisla.mendoza@gmail.com>
2021-07-21 22:42:09 -0500

    blank managment

---
+++ src/main/java/com/dlsys/sifood/ms/ManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dlsys.sifood.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlsys/sifood/ms/ManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlsys.sifood.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
